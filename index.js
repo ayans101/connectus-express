305820fe8229f04b5d23b621bb01a6741bb6cf64
Ayan Sarkar <ayan_s27@MacBook-Pro.local>
2021-02-12 17:16:42 +0530

    set up express session to use passport as a middleware to authenticate

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 const port = 8080;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+//  used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -19,6 +23,20 @@ app.set('layout extractScripts', true);
 //  set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.use(session({
+    name: 'connectUs',
+    // TODO change the secret before deployment in production mode
+    secret: 'something',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 60 * 8)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //  use express router
 app.use('/', require('./routes/index'));
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users_controller');
+const passport = require('passport');
 
 console.log('router loaded');
 
@@ -9,5 +10,11 @@ router.get('/sign-up', usersController.signup);
 router.get('/sign-in', usersController.signin);
 
 router.post('/create', usersController.create);
+
+//  use passport as a middleware to authenticate
+router.post('/create-session', passport.authenticate(
+    'local',
+    {failureRedirect: '/users/sign-in'}
+), usersController.createSession);
 
 module.exports = router;
