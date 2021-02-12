503d6753eee467fdcc98727730142f4f2a417337
Ayan Sarkar <ayan_s27@MacBook-Pro.local>
2021-02-12 16:50:00 +0530

    set up passport.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-//  use express router
-app.use('/', require('./routes/index'));
-
 //  set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+//  use express router
+app.use('/', require('./routes/index'));
 
 app.listen(port, function(err){
     if(err){
