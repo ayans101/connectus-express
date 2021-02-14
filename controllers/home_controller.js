c78b0b9739bb3acec52e2a0895001ea06ce20404
ayan <ayansarkar367@gmail.com>
2021-02-14 23:48:10 +0530

    update home_controller and home page to display posts and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,25 @@
+const Post = require("../models/post");
+
 module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 10);
-    return res.render('home', {
-        title: "Home"
+    // console.log(req.cookies);
+    // res.cookie('user_id', 10);
+    /*
+    Post.find({}, function(err, posts){
+        return res.render('home', {
+            title: "ConnectUs | Home",
+            posts: posts
+        });
     });
+    */
+
+    //  populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "ConnectUs | Home",
+            posts: posts
+        });
+    });
+    
 };
 
 // module.exports.actionName = function(req, res){};
