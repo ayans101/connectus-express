f92e999c94abf1b468b3d98e93ac8bc98587e299
ayan <ayansarkar367@gmail.com>
2021-02-17 05:04:12 +0530

    convert to async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,36 +1,32 @@
 const Post = require("../models/post");
 const User = require("../models/user");
 
-module.exports.home = function(req, res){
-    // console.log(req.cookies);
-    // res.cookie('user_id', 10);
-    /*
-    Post.find({}, function(err, posts){
+module.exports.home = async function(req, res){
+
+    try{
+
+        //  populate the user of each post
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+
+        let users = await User.find({});
+
         return res.render('home', {
             title: "ConnectUs | Home",
-            posts: posts
+            posts: posts,
+            all_users: users
         });
-    });
-    */
 
-    //  populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-        User.find({}, function(err, users){
-            return res.render('home', {
-                title: "ConnectUs | Home",
-                posts: posts,
-                all_users: users
-            });
-        });
-    });
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
     
 };
 
