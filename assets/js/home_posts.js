1aa6e257f442773bad1bce9665658ff15bcae391
ayan <ayansarkar367@gmail.com>
2021-02-18 23:14:24 +0530

    delete a post using ajax

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,5 +1,5 @@
-{
-    //  method to submit the form data for new post using AJAX
+{   
+    // method to submit the form data for new post using AJAX
     let createPost = function(){
         let newPostForm = $('#new-post-form');
 
@@ -13,6 +13,7 @@
                 success: function(data){
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-container>ul').prepend(newPost);
+                    deletePost($(' .delete-post-button', newPost));
                 }, error: function(err){
                     console.log(err.responseText);
                 }
@@ -20,37 +21,57 @@
         });
     };
 
-    //  method to create a post in DOM
+    // method to create a post in DOM
     let newPostDom = function(post){
-        return $(`<li id="post-${post.id}">
+        return $(`<li id="post-${post._id}">
                     <p>
-                            <small>
-                                <a id="delete-post-button" href="/posts/destroy/${post.id}">X</a>
-                            </small>
 
+                        <small>
+                            <a class="delete-post-button"  href="/posts/destroy/${ post._id }">X</a>
+                        </small>
+                       
                         ${ post.content }
                         <br>
                         <small>
-                            ${ post.user.name }
+                        ${ post.user.name }
                         </small>
                     </p>
                     <div class="post-comments">
-
+                        
                             <form action="/comments/create" method="POST">
                                 <input type="text" name="content" placeholder="Type Here to add comment..." required>
-                                <input type="hidden" name="post" value="${post._id}">
+                                <input type="hidden" name="post" value="${ post._id }" >
                                 <input type="submit" value="Add Comment">
                             </form>
-
+               
                 
                         <div class="post-comments-list">
-                            <ul id="post-comments-${post._id}">
+                            <ul id="post-comments-${ post._id }">
                                 
                             </ul>
                         </div>
                     </div>
+                    
                 </li>`);
     };
+
+    // method to delete a post from DOM
+    let deletePost = function(deleteLink){
+        $(deleteLink).click(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success: function(data){
+                    $(`#post-${data.data.post_id}`).remove();
+                },error: function(err){
+                    console.log(err.responseText);
+                }
+            });
+
+        });
+    }
 
     createPost();
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -5,7 +5,7 @@ module.exports.create = async function(req, res){
     try{
         let post = await Post.create({
             content: req.body.content,
-            user: req.user._id  //  current signed in user is already stored in the locals
+            user: req.user  //  current signed in user is already stored in the locals
         });
     
         if(req.xhr){
@@ -35,6 +35,15 @@ module.exports.destroy = async function(req, res){
             post.remove();
     
             await Comment.deleteMany({post: req.params.id});
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: 'Post deleted'
+                });
+            }
 
             req.flash('success', 'Post and associated comments deleted');
             return res.redirect('back');
