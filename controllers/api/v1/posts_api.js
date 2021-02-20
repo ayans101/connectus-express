a88241723db93b97b0a83a7e787c953e9381af30
ayan <ayansarkar367@gmail.com>
2021-02-21 04:50:14 +0530

    create an api to show and delete posts

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,41 @@
-module.exports.index = function(req, res){
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function(req, res){
+
+    let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+        }
+    });
+
     return res.json(200, {
         message: "Lists of posts",
-        posts: []
+        posts: posts
     });
+}
+
+module.exports.destroy = async function(req, res){
+
+    try{
+        let post = await Post.findById(req.params.id);
+
+        post.remove();
+    
+        await Comment.deleteMany({post: req.params.id});
+            
+        return res.json(200, {
+            message: "Post and associated comments deleted successfully"
+        });
+
+    }catch(err){
+        return res.json(500, {
+            message: "Internal Server Error"
+        });
+    }
+
 }
