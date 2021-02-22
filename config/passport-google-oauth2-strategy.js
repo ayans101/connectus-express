6d98c21e59e4b527f0cabeed08062550c7b74062
ayan <ayansarkar367@gmail.com>
2021-02-22 17:26:17 +0530

    create sign in using google auth

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -12,9 +12,10 @@ passport.use(new googleStrategy({
 
     function(accessToken, refreshToken, profile, done){
         //  find a user
-        User.findOne({email: profile.emails[0].value}).exec(function(err){
+        User.findOne({email: profile.emails[0].value}).exec(function(err, user){
             if(err){ console.log('error in google strategy-passport', err); return;}
 
+            console.log(accessToken, refreshToken);
             console.log(profile);
 
             if(user){
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const usersController = require('../controllers/users_controller');
 const passport = require('passport');
+
+const usersController = require('../controllers/users_controller');
 
 // console.log('router loaded');
 
@@ -20,5 +21,11 @@ router.post('/create-session', passport.authenticate(
 ), usersController.createSession);
 
 router.get('/sign-out', usersController.destroySession);
+
+router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+router.get('/auth/google/callback', passport.authenticate(
+    'google', 
+    {failureRedirect: '/users/sign-in'}
+), usersController.createSession);
 
 module.exports = router;
