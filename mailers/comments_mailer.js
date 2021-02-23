c3f5ff3e344aeb9cac9c7cf5b9609c6f5ec210ac
ayan <ayansarkar367@gmail.com>
2021-02-23 10:15:05 +0530

    create html template to send email

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,13 +1,13 @@
 const nodeMailer = require('../config/nodemailer');
 
 exports.newComment = (comment) => {
-    console.log('inside newComment mailer');
+    let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
 
     nodeMailer.transporter.sendMail({
         from: 'cindy.tromp@ethereal.email',
         to: comment.user.email,
         subject: "New Comment Published!",
-        html: '<h1>Yup, your comment is now published!</h1>'
+        html: htmlString
     }, (err, info) => {
         if(err){
             console.log('Error in sending mail', err);
@@ -18,27 +18,3 @@ exports.newComment = (comment) => {
         return;
     });
 }
-
-
-// const nodeMailer = require('../config/nodemailer');
-
-
-// // this is another way of exporting a method
-// exports.newComment = (comment) => {
-//     console.log('inside newComment mailer', comment);
-
-//     nodeMailer.transporter.sendMail({
-//        from: 'arpan@codingninjas.in',
-//        to: comment.user.email,
-//        subject: "New Comment Published!",
-//        html: '<h1>Yup, your comment is now published!</h1>' 
-//     }, (err, info) => {
-//         if (err){
-//             console.log('Error in sending mail', err);
-//             return;
-//         }
-
-//         console.log('Message sent', info);
-//         return;
-//     });
-// }
