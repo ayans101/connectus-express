c279c677b9ce214b1c18e709bda036259e0402f3
ayan <ayansarkar367@gmail.com>
2021-02-25 04:18:52 +0530

    set up schema for friendships

--- models/user.js
+++ models/user.js
@@ -20,8 +20,13 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String,
-
-    }
+    },
+    friendships: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Friendship'
+        }
+    ]
 }, {
     timestamps: true
 });
