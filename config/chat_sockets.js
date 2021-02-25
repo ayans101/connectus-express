b3b3b313020967db531ff20a6afa8a463450a268
ayan <ayansarkar367@gmail.com>
2021-02-25 23:41:45 +0530

    initiate socket connection between client and server

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,5 +1,18 @@
 
 
 module.exports.chatSockets = function(socketServer){
-    
+    let io = require('socket.io')(socketServer, {
+        cors: {
+            origin: 'http://localhost:8080',
+            credentials: true
+        }
+    });
+
+    io.sockets.on('connection', function(socket){
+        console.log('new connection received', socket.id);
+
+        socket.on('disconnect', function(){
+            console.log('socket disconnected!');
+        });
+    })
 }
