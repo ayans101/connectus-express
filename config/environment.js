e76a6773fe83e36866c85c8800312788bc0568f0
ayan <ayansarkar367@gmail.com>
2021-02-28 19:29:52 +0530

    set up production environment

--- config/environment.js
+++ config/environment.js
@@ -21,7 +21,24 @@ const development = {
 }
 
 const production = {
-    name: 'production'
+    name: 'production',
+    assets_path: process.env.CONNECTUS_ASSET_PATH,
+    session_cookie_key: process.env.CONNECTUS_SESSION_COOKIE_KEY,
+    db: process.env.CONNECTUS_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.CONNECTUS_GMAIL_USERNAME,
+             pass: process.env. CONNECTUS_GMAIL_PASSWORD
+        }
+    },
+    google_client_id: process.env.CONNECTUS_GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.CONNECTUS_GOOGLE_CLIENT_SECRET,
+    google_call_back_url: process.env.CONNECTUS_GOOGLE_CALLBACK_URL,
+    jwt_secret: process.env.CONNECTUS_JWT_SECRET
 }
 
-module.exports = development;
+module.exports = eval(process.env.CONNECTUS_ENVIRONMENT) == undefined ? development : eval(process.env.CONNECTUS_ENVIRONMENT);
