1357619cd99cb3f6e027802f67803343907b5e25
Ayan Sarkar <ayansarkar367@gmail.com>
2021-04-05 15:11:18 +0530

    update users_api

--- controllers/api/v1/users_api.js
+++ controllers/api/v1/users_api.js
@@ -17,7 +17,7 @@ module.exports.createSession = async function(req, res){
             message: "Sign in successful, here is your token, please keep it safe",
             success: true,
             data: {
-                token: jwt.sign(user.toJSON(), env.jwt_secret, {expiresIn: '100000'}),
+                token: jwt.sign(user.toJSON(), env.jwt_secret, {expiresIn: '1d'}),
                 user: {
                     name: user.name,
                     email: user.email,
@@ -49,7 +49,7 @@ module.exports.registerUser = function(req, res){
                         message: "Sign up successful, here is your token, please keep it safe",
                         success: true,
                         data: {
-                            token: jwt.sign(user.toJSON(), env.jwt_secret, {expiresIn: '100000'}),
+                            token: jwt.sign(user.toJSON(), env.jwt_secret, {expiresIn: '1d'}),
                             user: {
                                 name: user.name,
                                 email: user.email,
@@ -72,23 +72,39 @@ module.exports.update = async function(req, res){
 
     if(req.body.id == req.params.id){
         try{
-            await User.findByIdAndUpdate(req.body.id, req.body, function(err, user){
-                user.name = req.body.name;
-                if(req.body.password){
-                    user.password = req.body.password;
-                }
-                return res.json(200, {
-                    message: "User updated successfully",
-                    success: true,
-                    data: {
-                        token: jwt.sign(user.toJSON(), env.jwt_secret, {expiresIn: '100000'}),
-                        user: {
-                            name: user.name,
-                            email: user.email,
-                            _id: user._id
-                        }
+            await User.findById(req.body.id, function(err, usr){
+                const authHeader = req.headers['authorization']
+                const token = authHeader && authHeader.split(' ')[1];
+                console.log("****", token);
+
+                jwt.verify(token, env.jwt_secret, (_err, _usr) => {
+                    if (!_usr || _err || _usr.email != usr.email) {
+                        return res.json(401, {
+                            message: "User not authorized",
+                            success: false,
+                        });
+                    }else{
+                        User.findByIdAndUpdate(req.body.id, req.body, function(err, user){
+                            user.name = req.body.name;
+                            if(req.body.password){
+                                user.password = req.body.password;
+                            }
+                            user.save();
+                            return res.json(200, {
+                                message: "User updated successfully",
+                                success: true,
+                                data: {
+                                    token: jwt.sign(user.toJSON(), env.jwt_secret, {expiresIn: '1d'}),
+                                    user: {
+                                        name: user.name,
+                                        email: user.email,
+                                        _id: user._id
+                                    }
+                                }
+                            });
+                        }) 
                     }
-                });
+                })
             });
         }catch(err){
             console.log(err);
@@ -96,6 +112,7 @@ module.exports.update = async function(req, res){
                 message: "Internal Server Error"
             });
         }
+
     }else{
         return res.json(401, {
             message: "Unauthorized",
